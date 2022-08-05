a0764b2bb80cdfabe4ca5d5f33d508cf7f63d24c
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-08-05 20:37:49 +0300

    did task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
 
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
